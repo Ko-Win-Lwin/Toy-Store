12fc09b544f1bee6ee6c9b25a3112853e0fcc1fb
Ko-Win-Lwin <winlwin.mce@gmail.com>
2023-12-29 12:34:47 +0630

    Stuffed and Wooden toys

---
+++ src/components/catelog/item-card.component.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mt-12 py-6 w-[340px] lg:w-[270px] hover:scale-110 rounded-lg shadow-lg duration-500">
+
+    <div class="flex flex-col justify-center items-center ">
+        <slot name="product_image"></slot>
+
+        <div class="mt-5 flex flex-col justify-center items-center">
+            <slot name="product_name"></slot>
+            <slot name="price_tag"></slot>
+        </div>
+    </div>
+    
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/catelog/wooden-toys.component.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="mt-40">
+    <div class="flex justify-between items-center">
+        <h1 class="text-3xl">Wooden Toys</h1>
+        <div class="group cursor-pointer" @click="gotoCatelogPage">
+            <span class="text-sm">See All Toys 	&rarr;</span>
+            <div class="w-full bg-gray-200 rounded-full h-[2px] group-hover:bg-lime-500" />
+        </div>
+    </div>
+    
+    <div class="w-full mt-8 bg-gray-200 rounded-full h-[2px] lg:h-[1.5px]">
+        <div class="bg-lime-500 h-[2px] lg:h-[1.5px] rounded" style="width: 15%" />
+    </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:gap-8">
+        <ItemCard>
+            <template #product_image>
+                <img src="../../assets/images/wooden_toys/wooden_spinner.png" alt="" class="w-[200px]">
+            </template>
+
+            <template #product_name>
+              <span>Happy Flower</span>
+            </template>
+
+            <template #price_tag>
+                <PriceTag>
+                    <template #price_tag>$ 30.00 USD</template>
+                </PriceTag>
+            </template>
+        </ItemCard>
+
+        <ItemCard>
+            <template #product_image>
+                <img src="../../assets/images/wooden_toys/wooden_spinner.png" alt="" class="w-[200px]">
+            </template>
+
+            <template #product_name>
+              <span>Happy Flower</span>
+            </template>
+
+            <template #price_tag>
+                <PriceTag>
+                    <template #price_tag>$ 30.00 USD</template>
+                </PriceTag>
+            </template>
+        </ItemCard>
+
+        <ItemCard>
+            <template #product_image>
+                <img src="../../assets/images/wooden_toys/wooden_spinner.png" alt="" class="w-[200px]">
+            </template>
+
+            <template #product_name>
+              <span>Happy Flower</span>
+            </template>
+
+            <template #price_tag>
+                <PriceTag>
+                    <template #price_tag>$ 30.00 USD</template>
+                </PriceTag>
+            </template>
+        </ItemCard>
+
+        <ItemCard>
+            <template #product_image>
+                <img src="../../assets/images/wooden_toys/wooden_spinner.png" alt="" class="w-[200px]">
+            </template>
+
+            <template #product_name>
+              <span>Happy Flower</span>
+            </template>
+
+            <template #price_tag>
+                <PriceTag>
+                    <template #price_tag>$ 30.00 USD</template>
+                </PriceTag>
+            </template>
+        </ItemCard>
+
+
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+import ItemCard from "./item-card.component.vue"
+import PriceTag from "../btns/price-tag.component.vue"
+import router from "@/router";
+
+const gotoCatelogPage = () => {
+  router.push({"name": "catelog"});
+}
+</script>
+
+<style>
+
+</style>
